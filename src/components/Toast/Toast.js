16625fed10a8dbd362a50550f1aaa5859e3645d3
Dave Welch <dave.w@velocitypartners.com>
2023-10-29 21:16:04 +0000

    add solution to exercise 5.2

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 import { ToastContext } from "../ToastProvider";
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,22 +18,24 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast( {variant, children, id }) {
-
-    const {removeToast} = React.useContext( ToastContext );
-    const Icon = ICONS_BY_VARIANT[ variant ];
+function Toast({ variant, children, id }) {
+  const { removeToast } = React.useContext(ToastContext);
+  const Icon = ICONS_BY_VARIANT[variant];
 
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
         <Icon size={24} />
+        <VisuallyHidden>{variant} - </VisuallyHidden>
       </div>
-      <p className={styles.content}>
-        {children}
-      </p>
-      <button className={styles.closeButton}  onClick={ () => removeToast( id ) }>
-        <X size={24}/>
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
+      <p className={styles.content}>{children}</p>
+      <button
+        className={styles.closeButton}
+        onClick={() => removeToast(id)}
+        aria-label="Dismiss message"
+        aria-live="off"
+      >
+        <X size={24} />
       </button>
     </div>
   );
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
 import { ToastContext } from "../ToastProvider";
 function ToastShelf() {
-    const {toasts} = React.useContext( ToastContext );
+  const { toasts } = React.useContext(ToastContext);
 
   return (
-    <ol className={styles.wrapper}>
-        { toasts.map( ({message, variant, id }) => (
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
+      {toasts.map(({ message, variant, id }) => (
         <li className={styles.toastWrapper} key={id}>
-            <Toast variant={variant} id={id}>{message}</Toast>
+          <Toast variant={variant} id={id}>
+            {message}
+          </Toast>
         </li>
-        ))}
+      ))}
     </ol>
   );
 }
